5941a969aac60024151a42ab739e95c72fa34412
JJTB Somhorst <j.somhorst@gmail.com>
2022-02-05 08:04:59 +0100

    removed index for safety reasons..

--- src/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('OmdbApiClient.php');
-use jjtbsomhorst\Omdbapi\OmdbApiClient;
-require_once __DIR__ . '/vendor/autoload.php';
-
-$cl = (new OmdbApiClient())->apiKey("62302d61");
-$response = $cl->searchRequest('Harry Potter','1')->execute(true);
